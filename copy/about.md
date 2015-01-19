6e2d4805334cbf463a59c826041d28d450f3fdb7
gregtandiono <gregtandiono@gmail.com>
2015-01-19 00:24:33 -0800

    added libs and submodules

---
+++ copy/about.md
@@ -0,0 +1,49 @@
+# Greg Tandiono
+## Full Stack Developer.
+
+
+### Skills
+
+**Server-Side Development:**
+
+- Javascript
+  - Node
+  - ExpressJs
+  - Firebase
+- Ruby
+  - Ruby on Rails
+  - Sinatra
+- Golang
+- PHP (once upon a time)
+
+
+**Client-Side Development:**
+
+- React w/ Flux
+- Backbone
+- Angular
+- CommonJs
+  - Browserify
+  - RequireJs
+- Grunt
+- Bower
+- CSS
+  - Less
+  - Stylus
+  - Sass
+  - framework agnostic
+
+
+**Others:**
+
+- Ansible
+- Jasmine
+- Jest (jasmine wrapper for react components)
+- Git for version control
+- Travis for CI
+- Wercker for CI & AD (automated deployment after test)
+- Capistrano for automated deployment
+- Very comfortable in linux environment
+- Love Zsh
+- Highly proficient in using Pivotal Tracker
+- Text Editor of choice: **VIM** all the way
--- javascripts/react-components/views/home.jsx
+++ javascripts/react-components/views/home.jsx
@@ -1,13 +1,15 @@
-var React    = require('react')
-  , velocity = require('velocity-animate')
-  , _        = require('underscore');
+var React       = require('react')
+  , svgInjector = require('svg-injector')
+  , _           = require('underscore');
 
 var Home = React.createClass({
   render: function() {
     return (
-      <div className='container'>
-        <h1>Hello, world</h1>
-      </div>
+      <section className='hero' id='home'>
+        <div className='container'>
+          <AnimatedObj />
+        </div>
+      </section>
     )
   }
 });
@@ -17,6 +19,36 @@ var Hero = React.createClass({
     return (
       <div className='hero'>
       </div>
+    )
+  }
+});
+
+var AnimatedObj = React.createClass({
+  clickHandler: function() {
+    var mainObj = $('#animated-obj')
+      , bottomBox = $('g#bottom')
+      , middleBox = $('g#middle');
+
+    var animationSequence = [
+      { e: mainObj, p: { translateY: '60px' } },
+      { e: mainObj, p: { translateX: '60px' } },
+      { e: bottomBox, p: { translateY: '80px' } }
+    ];
+
+    $.Velocity.RunSequence(animationSequence);
+  },
+  componentDidMount: function() {
+    svgInjector($('.svg-inject'));
+  },
+  render: function() {
+    return (
+      <img
+        src='/images/boxes.svg'
+        onClick={this.clickHandler}
+        id='animated-obj'
+        ref='fucking'
+        className='svg-inject'
+      />
     )
   }
 });
